aa32f0cbe528c3213e946ad25996e3c0cb7ba995
denislobur <denisdenisdenis@bigmir.net>
2019-08-21 16:01:09 +0300

    Start working on StopWatch projects

---
+++ src/StopWatch.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class StopWatch extends JFrame {
+    public static void main(String[] args) {
+        new StopWatch().show();
+    }
+
+    StopWatch(){
+        setTitle("Stop watch");
+        setSize(300, 300);
+    }
+}
